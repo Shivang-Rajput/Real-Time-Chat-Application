1cd15198b6a4ed8de0d56ded18495d0ef0168afa
Shivang_Rajput <singhshivang765@gmail.com>
2026-07-21 23:48:59 +0530

    testing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://real-time-chat-application-backend-mprn.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
